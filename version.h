e02ccfc79d0a4fe8c7087529faf2f76d210993df
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2021-01-14 17:37:58 -0800

    Change the version number

--- version.h
+++ version.h
@@ -18,8 +18,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
 
-static int skampi_maj_version = 5 ;
+static int skampi_maj_version = 6 ;
 static int skampi_min_version = 0 ;
-static int skampi_sub_release = 4 ;
+static int skampi_sub_release = 1 ;
 
 static char svn_revision[]="355";
