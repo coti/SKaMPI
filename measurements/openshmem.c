fdde093818779637ee12406f8fd603f4c6912fed
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2021-02-03 17:50:34 -0800

    Wait until

--- measurements/openshmem.c
+++ measurements/openshmem.c
@@ -243,6 +243,37 @@ double measure_Shmem_Fence_Put( int count, int iterations ){
 }
 
 /*---------------------------------------------------------------------------*/
+/*                         Point-to-pont synchronization                     */
+/*---------------------------------------------------------------------------*/
+
+/* Just wait for something which is already there */
+
+void init_Shmem_Wait_Until( int iterations ){
+    target = (char*) shmalloc( sizeof( int ) );
+    init_synchronization();
+}
+
+void finalize_Shmem_Wait_Until( int iterations ){
+    shfree( target );
+    target = NULL;
+}
+
+double measure_Shmem_Wait_Until( int iterations ){
+    double start_time = 1.0, end_time = 0.0;
+    int i;
+    int* a = (int*) target;
+    *a = 1;
+    
+    start_time = start_synchronization();
+    for( i = 0 ; i < iterations ; i++ ) {
+        shmem_int_wait_until( a, SHMEM_CMP_EQ, 1 );
+    }
+    end_time = stop_synchronization();
+    
+    return (end_time - start_time)/iterations;
+}
+
+/*---------------------------------------------------------------------------*/
 
 #pragma weak end_skampi_extensions
 
